25140dbc45d9b5ae754f9babbc9ef1666fdae4b1
ryan <ryan.megarity@immerse.io>
2024-12-10 14:47:09 +0000

    Use X11 to support zoom sharing

--- configuration.nix
+++ configuration.nix
@@ -47,6 +47,8 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  services.xserver.displayManager.gdm.wayland = false;
+
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
