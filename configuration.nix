ee71e8d309608d5c6ae9583f3a748ca989dc2e10
Ryan Megarity <ryan.megarity@immerse.io>
2024-12-11 13:19:35 +0000

    Revert "Use X11 to support zoom sharing"
    This reverts commit 25140dbc45d9b5ae754f9babbc9ef1666fdae4b1.

--- configuration.nix
+++ configuration.nix
@@ -60,8 +60,6 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  services.xserver.displayManager.gdm.wayland = false;
-
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
