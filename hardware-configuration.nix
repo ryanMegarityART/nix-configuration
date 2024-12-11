dddc4d7bbc92cd6c48bd4d860adcc5f1e508a28b
Ryan Megarity <ryan.megarity@immerse.io>
2024-12-11 11:46:42 +0000

    Add swap

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
@@ -14,19 +15,24 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/fe065f3c-c520-4fa5-a8ff-eeb51cf8e940";
+    {
+      device = "/dev/disk/by-uuid/fe065f3c-c520-4fa5-a8ff-eeb51cf8e940";
       fsType = "ext4";
     };
 
   boot.initrd.luks.devices."luks-9ccbef83-cdae-428d-a2ae-6135c651ba8a".device = "/dev/disk/by-uuid/9ccbef83-cdae-428d-a2ae-6135c651ba8a";
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/D6E3-3509";
+    {
+      device = "/dev/disk/by-uuid/D6E3-3509";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [{
+    device = "/var/lib/swapfile";
+    size = 16 * 1024; # 16gb
+  }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
